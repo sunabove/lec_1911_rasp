f24c9212637e6ec758515db61503dec952ccd66c
sunabove <sunabove@users.noreply.github.com>
2019-03-25 22:49:10 +0900

    **

--- blink.py
+++ blink.py
@@ -1,32 +1,18 @@
 # coding: utf-8
 
-import time
-import RPi.GPIO as GPIO
+from gpiozero import LED
+from time import sleep
 
-# Pin definitions
-led_pin = 17
+led = LED(17)
 
-# Suppress warnings
-GPIO.setwarnings(False)
-
-# Use "GPIO" pin numbering
-GPIO.setmode(GPIO.BCM)
-
-# Set LED pin as output
-GPIO.setup(led_pin, GPIO.OUT)
-
-try:
-    # Blink forever
-    while True:
-        print("+", end = '', flush=True )
-        GPIO.output(led_pin, GPIO.HIGH) # Turn LED on
-        time.sleep(1)                   # Delay for 1 second
-        print("\b-", end = '', flush=True)
-        GPIO.output(led_pin, GPIO.LOW)  # Turn LED off
-        time.sleep(1)                   # Delay for 1 second
-        print("\b", end = '', flush=True)
-    pass
-# 예외 검출
-except Exception :
-    print( "\nExit program." )
+print( "Press Ctrl + C to quit! ")
+while True:
+    led.on()
+    print("+", end = '', flush=True )
+    sleep(1)
+    led.off()
+    print("\b-", end = '', flush=True)
+    sleep(1)
+    print("\b", end = '', flush=True)
 pass
+ 
