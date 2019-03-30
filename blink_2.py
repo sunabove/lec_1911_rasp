126deddeeebddfe7c620fda1e6304acae327dfaf
sunabove <sunabove@users.noreply.github.com>
2019-03-31 06:43:39 +0900

    **

--- blink_2.py
+++ blink_2.py
@@ -15,18 +15,13 @@ GPIO.setmode(GPIO.BCM)
 # Set LED pin as output
 GPIO.setup(led_pin, GPIO.OUT)
 
-try:
-    # Blink forever
-    while True:
-        print("+", end = '', flush=True )
-        GPIO.output(led_pin, GPIO.HIGH) # Turn LED on
-        time.sleep(1)                   # Delay for 1 second
-        print("\b-", end = '', flush=True)
-        GPIO.output(led_pin, GPIO.LOW)  # Turn LED off
-        time.sleep(1)                   # Delay for 1 second
-        print("\b", end = '', flush=True)
-    pass
-# 예외 검출
-except Exception :
-    print( "\nExit program." )
+# Blink forever
+while True:
+    print("+", end = '', flush=True )
+    GPIO.output(led_pin, GPIO.HIGH) # Turn LED on
+    time.sleep(1)                   # Delay for 1 second
+    print("\b-", end = '', flush=True)
+    GPIO.output(led_pin, GPIO.LOW)  # Turn LED off
+    time.sleep(1)                   # Delay for 1 second
+    print("\b", end = '', flush=True)
 pass
