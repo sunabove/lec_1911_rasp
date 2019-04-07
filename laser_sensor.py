cebf93765ca4fe1aa01b2a1e081e646911818469
sunabove <sunabove@nate.com>
2019-04-07 15:29:03 -0400

    **

---
+++ laser_sensor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+from gpiozero import *
+from time import sleep
+
+sensor = MotionSensor( 4 )
+
+idx = 0 ; 
+while 1 :
+    idx += 1 
+    print( "[%03d] : Waiting for motion ..." % idx )
+    sensor.wait_for_motion()
+    print( "[%03d] : It's light! :)" % idx  ) 
+    sleep( 2 )
+pass
