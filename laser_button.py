8c3b8240424f8097cf8e4ff8fd1a277aa5742384
sunabove <sunabove@nate.com>
2019-04-08 01:00:07 -0400

    **

---
+++ laser_button.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+from gpiozero import MotionSensor, LED
+from signal import pause
+
+pir = MotionSensor(4)
+led = LED(16)
+
+pir.when_motion = led.on
+pir.when_no_motion = led.off
+
+pause()
