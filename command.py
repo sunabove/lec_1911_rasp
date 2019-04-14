e904f4d69431f7f77e178fc9a63ec69d90a9f7e6
sunabove <sunabove@nate.com>
2019-04-14 18:50:07 -0400

    **

---
+++ command.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+from guizero import App, Text, PushButton
+
+def say_hello():
+    text.value = "hello world"
+
+app = App()
+text = Text(app)
+button = PushButton(app, command=say_hello)
+app.display()
