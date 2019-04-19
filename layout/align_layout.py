3fa52886232a53a2df3a799f110e727a1c1810c0
sunabove <sunabove@nate.com>
2019-04-19 12:25:36 -0400

    **

--- layout/align_layout.py
+++ layout/align_layout.py
@@ -1,7 +1,8 @@
 # coding: utf-8
-from guizero import App, Box, Text
-app = App(title="My app", height=300, width=400)
-box = Box(app)
-text1 = Text(box, text="Hello from the box", size=14, color="red", font="Arial")
-text2 = Text(app, text="Hello from the app", size=14, color="blue", font="Courier New")
+from guizero import App, Text
+app = App()
+top_text = Text(app, text="at the top", align="top")
+bottom_text = Text(app, text="at the bottom", align="bottom")
+left_text = Text(app, text="to the left", align="left")
+right_text = Text(app, text="to the right", align="right")
 app.display()
